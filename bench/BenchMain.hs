5e42db26ab02a57ca9c0ffd4fd633a55d9185380
Ryo Ota <nwtgck@gmail.com>
2017-08-27 09:54:59 +0900

    [change] Chage BenchMain.hs to use criterion

--- bench/BenchMain.hs
+++ bench/BenchMain.hs
@@ -1,3 +1,10 @@
+import Criterion.Main
+
 import Data.String.Strip
 
-main = putStrLn "here is benchmark!!"
+main = defaultMain [
+  bgroup "strip" [ bench "short"  $ whnf strip " aaaaa      "
+                 , bench "middle"  $ whnf strip " aaaaaaaaaaa      "
+                 , bench "long"  $ whnf strip "  aaaaaaaaaaaaaaaaaaaaabababababababababbabababbababbabababbababab           "
+                 ]
+  ]
