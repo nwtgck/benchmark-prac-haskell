22f0bef9fd804f4294b2b4648c354f6cb7834257
Ryo Ota <nwtgck@gmail.com>
2017-08-27 10:16:20 +0900

    [change] Change benchmark to use `replicate`

--- bench/BenchMain.hs
+++ bench/BenchMain.hs
@@ -1,10 +1,37 @@
 import Criterion.Main
 
 import Data.String.Strip
+import Text.Printf
 
-main = defaultMain [
-  bgroup "strip" [ bench "short"  $ whnf strip " aaaaa      "
-                 , bench "middle"  $ whnf strip " aaaaaaaaaaa      "
-                 , bench "long"  $ whnf strip "  aaaaaaaaaaaaaaaaaaaaabababababababababbabababbababbabababbababab           "
-                 ]
-  ]
+main = let
+  startWhiteSpaces :: String
+  startWhiteSpaces = "   "
+
+  endWhiteSpaces :: String
+  endWhiteSpaces = " \t   "
+
+  short    = 5
+  middle   = 50
+  long     = 100
+  veryLong = 1000
+
+  shortStr :: String
+  shortStr = startWhiteSpaces ++ replicate short 'a' ++ endWhiteSpaces
+
+  middleStr :: String
+  middleStr = startWhiteSpaces ++ replicate middle 'a' ++ endWhiteSpaces
+
+  longStr :: String
+  longStr = startWhiteSpaces ++ replicate long 'a' ++ endWhiteSpaces
+
+  veryLongStr :: String
+  veryLongStr = startWhiteSpaces ++ replicate veryLong 'a' ++ endWhiteSpaces
+
+  in defaultMain [
+       bgroup "strip" [ bench "foo bar"   $ whnf strip " foo bar     "
+                      , bench (printf "short (len:%d)" short)         $ whnf strip shortStr
+                      , bench (printf "middle (len:%d)" middle)       $ whnf strip middleStr
+                      , bench (printf "long (len: %d)" long)          $ whnf strip longStr
+                      , bench (printf "very long (len: %d)" veryLong) $ whnf strip veryLongStr
+                      ]
+       ]
